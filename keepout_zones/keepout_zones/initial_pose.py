27b39317141d9982c1e000cf6879580e50a25b57
Mo-AH <91679281+Mo-AH@users.noreply.github.com>
2022-07-11 23:38:36 +0200

    Delete keepout_zones directory

--- keepout_zones/keepout_zones/initial_pose.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main():
-    os.system ( "ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {stamp: {sec: 0, nanosec: 0}, frame_id: 'map'}, pose: { pose: {position: {x: 0.1, y: 0.0, z: 0.0}, orientation: {w: 0.1}}, } }' ")
-
-if __name__ == '__main__':
-    main()
